faa24b5b7cb100aa0219801709757f3e2baeaf23
Daniel Sanabria <sanabriabrasil@hotmail.com>
2026-04-20 13:32:40 -0300

    Initial Commit

---
+++ tca architecture/tca_architectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  tca_architectureApp.swift
+//  tca architecture
+//
+//  Created by Daniel Sanabria on 20/04/26.
+//
+
+import SwiftUI
+
+@main
+struct tca_architectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
