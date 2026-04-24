8f6c49e4699c03f30c49d1f9606224ea039dccb1
Daniel Sanabria <sanabriabrasil@hotmail.com>
2026-04-24 15:29:55 -0300

    DEV - Tests Setup - Create Tests Scheme - tca archictectureTests.xcsheme

---
+++ tca architectureTests/tca_architectureTests.swift
@@ -0,0 +1,28 @@
+//
+//  tca_architectureTests.swift
+//  tca architectureTests
+//
+//  Created by Daniel Sanabria on 24/04/26.
+//
+
+import ComposableArchitecture
+import Testing
+
+@testable import tca_architecture
+
+@MainActor
+struct tca_architectureTests {
+
+    @Test func basics() async throws {
+        let store = TestStore(initialState: CounterFeature.State()) {
+            CounterFeature()
+        }
+        await store.send(.incrementButtonTapped){
+            $0.count = 1
+        }
+        await store.send(.decrementButtonTapped){
+            $0.count = 0
+        }
+    }
+
+}
