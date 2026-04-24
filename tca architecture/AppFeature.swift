587c0574102dbe5c2c059b6816b53b18c3e84f9f
Daniel Sanabria <sanabriabrasil@hotmail.com>
2026-04-24 18:24:19 -0300

    DEV - Composing features Section3 - Edit feature to use AppFeature StoreOf - AppFeature.swift

--- tca architecture/AppFeature.swift
+++ tca architecture/AppFeature.swift
@@ -9,15 +9,14 @@ import ComposableArchitecture
 import SwiftUI
 
 struct AppView: View {
-    let store1: StoreOf<CounterFeature>
-    let store2: StoreOf<CounterFeature>
+    let store: StoreOf<AppFeature>
     
     var body: some View {
         TabView {
-            CounterView(store: store1).tabItem {
+            CounterView(store: store.scope(state: \.tab1, action: \.tab1)).tabItem {
                 Text("Counter 1")
             }
-            CounterView(store: store2).tabItem {
+            CounterView(store: store.scope(state: \.tab2, action: \.tab2)).tabItem {
                 Text("Counter 2")
             }
         }
@@ -46,4 +45,11 @@ struct AppFeature {
       return .none
     }
   }
+}
+#Preview {
+  AppView(
+    store: Store(initialState: AppFeature.State()) {
+      AppFeature()
+    }
+  )
 }
