76c573bbccf21a606c686bb7260a49fe55180850
Daniel Sanabria <sanabriabrasil@hotmail.com>
2026-04-24 17:44:43 -0300

    DEV - Tests Section4 - Use numberFact TCA Interface pattern for Test request with mock - CounterFeature.swift

--- tca architectureTests/tca_architectureTests.swift
+++ tca architectureTests/tca_architectureTests.swift
@@ -48,12 +48,16 @@ struct tca_architectureTests {
     @Test func numberFact() async throws {
         let store = TestStore(initialState: CounterFeature.State()) {
             CounterFeature()
+        } withDependencies: {
+            $0.numberFact.fetch = { "\($0) is a good number."}
         }
+        
         await store.send(.factButtonTapped) {
             $0.isLoading = true
         }
         await store.receive(\.factResponse, timeout: .seconds(1)) {
             $0.isLoading = false
+            $0.fact = "0 is a good number."
         }
     }
 }
