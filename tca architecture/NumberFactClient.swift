0aae509d31b2e64b68d099f56177341acc147da5
Daniel Sanabria <sanabriabrasil@hotmail.com>
2026-04-30 13:46:17 -0300

    DEV - Rename Client and property - NumberFactClient.swift

--- tca architecture/NumberFactClient.swift
+++ tca architecture/NumberFactClient.swift
@@ -8,12 +8,12 @@ import ComposableArchitecture
 import Foundation
 
 struct NumberFactClient {
-    var fetch: (Int) async throws -> String
+    var fact: (Int) async throws -> String
 }
 
 extension NumberFactClient: DependencyKey {
     static let liveValue = Self(
-        fetch: { number in
+        fact: { number in
             let (data, _) = try await URLSession.shared.data(from: URL(string: "http://number-trivia.com/\(number)")!)
             return String(decoding: data, as:UTF8.self)
         }
@@ -21,7 +21,7 @@ extension NumberFactClient: DependencyKey {
 }
 
 extension DependencyValues {
-    var numberFact: NumberFactClient {
+    var numberFactClient: NumberFactClient {
         get { self[NumberFactClient.self]}
         set { self[NumberFactClient.self] = newValue}
     }
